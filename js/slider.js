8a60a4b9e28d0f103301f4d58f7c917533b0f5d3
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-17 18:55:19 +0600

    js custom slider

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,19 @@
+const images = [
+    'images/banner_01.png',
+    'images/banner_02.png',
+    'images/banner_03.png',
+    'images/banner_04.png',
+    'images/banner_05.png',
+];
+
+let imgIndex = 0;
+const img = document.getElementById('slider_image');
 setInterval( ()=>{
-    console.log('hwllo')
-} ,1000)
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const imgLink = images[imgIndex];
+    // console.log(imgLink)
+    img.setAttribute('src', imgLink)
+    imgIndex++;
+} ,3000) 
